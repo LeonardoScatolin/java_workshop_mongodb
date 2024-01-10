66f8791d76278591f6de216d1a62ea0691249db2
LeonardoScatolin <leonardo.scatolin20@gmail.com>
2024-01-10 20:27:14 -0300

    Consulta com vários critérios

--- src/main/java/com/leonardo/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/leonardo/workshopmongo/repository/PostRepository.java
@@ -5,14 +5,21 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+    // "Buscar posts contendo um dado string no título"
     @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
     List<Post> findByTitle(String text);
 
+    // "Buscar posts contendo um dado string no título"
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    // "Buscar posts contendo um dado string em qualquer lugar (no título, corpo ou comentários) e em um dado intervalo de datas"
+    @Query("{ $and: [ { date: { $gte: ?1 } }, { date: { $lte: ?2 } }, { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
 
 }
